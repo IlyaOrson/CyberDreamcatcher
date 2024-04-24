4b30905f0616d2a14c968b55e9625d1bae433f4f
Ilya Orson Sandoval <ilya.orson@gmail.com>
2024-04-24 02:37:34 +0100

    Use hydra to manage simulation and training results

--- blueskynet/training/reinforce.py
+++ blueskynet/training/reinforce.py
@@ -12,13 +12,13 @@ EPS = np.finfo(np.float32).eps.item()
 
 
 @dataclass
-class Conf:
-    scenario: str
-    episode_length: int
-    num_episodes_sample: int
-    seed: int
-    learning_rate: float
-    optimizer_iterations: int
+class Config:
+    scenario: str = "Scenario2_-_User2_User4"  # "Scenario2_+_User5_User6"
+    episode_length: int = 30
+    num_episodes_sample: int = 100
+    seed: int = 0
+    learning_rate: float = 1e-3
+    optimizer_iterations: int = 1000
 
 
 class REINFORCE:
@@ -78,20 +78,30 @@ class REINFORCE:
 
         log_prob_R = 0.0
         for epi in range(num_episodes):
-
             # TODO is normalizing a valid baseline?
-            reward_to_go_baselined = (batch_rewards_to_go[epi] - reward_mean) / (reward_std + EPS)
+            reward_to_go_baselined = (batch_rewards_to_go[epi] - reward_mean) / (
+                reward_std + EPS
+            )
 
             # invert signs to maximize reward
-            log_prob_R -= torch.sum(torch.mul(torch.stack(batch_log_probs[epi]), torch.tensor(reward_to_go_baselined)))
-            for log_prob, reward_to_go in zip(batch_log_probs[epi], reward_to_go_baselined):
+            log_prob_R -= torch.sum(
+                torch.mul(
+                    torch.stack(batch_log_probs[epi]),
+                    torch.tensor(reward_to_go_baselined),
+                )
+            )
+            for log_prob, reward_to_go in zip(
+                batch_log_probs[epi], reward_to_go_baselined
+            ):
                 log_prob_R -= log_prob * reward_to_go
 
         mean_log_prob_R = log_prob_R / num_episodes
         return mean_log_prob_R, reward_mean, reward_std
 
     def learn(self):
-        optimizer = torch.optim.Adam(policy.parameters(), lr=self.conf.learning_rate)
+        optimizer = torch.optim.Adam(
+            self.policy.parameters(), lr=self.conf.learning_rate
+        )
 
         pbar = trange(self.conf.optimizer_iterations, desc="Optimizer iteration")
         for _ in pbar:
@@ -103,35 +113,43 @@ class REINFORCE:
 
             pbar.write(f"Roll-out mean reward: {reward_mean:.3} +- {reward_std:.2}")
 
-        return policy.state_dict()
+        return self.policy.state_dict()
 
 
 if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    import hydra
+    from hydra.core.config_store import ConfigStore
 
     from blueskynet.env import GraphWrapper
     from blueskynet.policy import Police
 
-    conf = Conf(
-        scenario="Scenario2_-_User2_User4",  # "Scenario2_+_User5_User6"
-        episode_length=30,
-        num_episodes_sample=100,
-        seed=0,
-        learning_rate=1e-2,
-        optimizer_iterations=1000,
-    )
+    cs = ConfigStore.instance()
+    # Registering the Config class with the name 'config'.
+    cs.store(name="config", node=Config)
 
-    env = GraphWrapper(scenario=conf.scenario, max_steps=conf.episode_length)
+    @hydra.main(version_base=None, config_name="config")
+    def app(cfg: Config) -> None:
+        print(f"Working directory : {os.getcwd()}")
+        output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+        print(f"Output directory  : {output_dir}")
 
-    policy = Police(env, latent_node_dim=env.host_embedding_size)
+        env = GraphWrapper(scenario=cfg.scenario, max_steps=cfg.episode_length)
 
-    trainer = REINFORCE(env, policy, conf)
+        policy = Police(env, latent_node_dim=env.host_embedding_size)
 
-    # test sampling
-    mean_log_prob_R, reward_mean, reward_std = trainer.sample_episodes()
+        trainer = REINFORCE(env, policy, cfg)
 
-    params_dict = trainer.learn()
+        # test sampling
+        mean_log_prob_R, reward_mean, reward_std = trainer.sample_episodes()
 
-    # store trained policy
-    # torch.save(policy.state_dict(), policy_path)
+        params_dict = trainer.learn()
 
-    print("Voila!")
+        # store trained policy
+        torch.save(policy.state_dict(), Path(output_dir) / "trained_params.pt")
+
+        print("Voila!")
+
+    app()
